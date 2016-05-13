d3c00e0c106abef22f2cc11a73cc684839fdd832
James K <james@gtoet.com>
2016-05-14 10:26:41 +1200

    every call

--- src/main/scala/org/leanpoker/player/Player.scala
+++ src/main/scala/org/leanpoker/player/Player.scala
@@ -1,6 +1,7 @@
 package org.leanpoker.player
 
-import play.api.libs.json.JsValue
+import play.api.libs.json._
+import play.api.libs.json.Json._
 
 object Player {
 
@@ -8,9 +9,16 @@ object Player {
 
   def betRequest(request: JsValue) = {
     //request.getAsJsonObject.get
+    val currentBuyIn =  (request \ "current_buy_in").as[JsNumber].value.toInt
+    val inAction = (request \ "in_action").as[JsNumber].value.toInt
     
+    val players = (request \ "players").as[JsArray]
+    val activePlayerBet = (players(inAction) \ "bet").as[JsNumber].value.toInt
     
-   100 
+    val callAmount = currentBuyIn - activePlayerBet
+    val minimumRaise = (request \ "minimum_raise").as[JsNumber].value.toInt
+    
+   callAmount
   }
 
   def showdown(game: JsValue) {

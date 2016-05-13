69a8c77465e17f5ef70cc7b8dbd905c33ff73249
Dennis Sosnoski <dms@sosnoski.com>
2016-05-14 10:06:16 +1200

    Initial bet

--- src/main/scala/org/leanpoker/player/Player.scala
+++ src/main/scala/org/leanpoker/player/Player.scala
@@ -6,7 +6,7 @@ object Player {
 
   val VERSION = "Default Scala folding player"
 
-  def betRequest(request: JsonElement) = 0
+  def betRequest(request: JsonElement) = 100
 
   def showdown(game: JsonElement) {
 
